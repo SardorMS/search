8317e000870805c240faf76cbd15b34de67eb725
SardorMS <mshinodawindows@gmail.com>
2021-05-02 19:11:32 +0500

    add func

---
+++ cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"math/rand"
+
+	// "sync"
+	"time"
+)
+
+func main() {
+	root := context.Background()
+	ctx, cancel := context.WithCancel(root)
+	ch := make(chan int)
+
+	// log.Print(ctx.Deadline())
+	// <-ctx.Done()
+	// log.Print(ctx.Deadline())
+	// log.Print(ctx.Err())
+	// log.Print(ctx.Err() == context.DeadlineExceeded)
+
+	for i := 0; i < 5; i++ {
+		go func(ctx context.Context, index int, ch chan<- int) {
+			wait := rand.Intn(10)
+			log.Printf("%d goroutine wait %d-second", index, wait)
+			select {
+			case <-ctx.Done():
+				log.Printf("%d canceled", index)
+			case <-time.After(time.Second * time.Duration(wait)):
+				ch <- index
+				log.Printf("%d goroutine done with %d-seconds ", index, wait)
+			}
+		}(ctx, i, ch)
+	}
+
+	winner := <-ch
+	cancel()
+	log.Print(winner)
+	<-time.After(time.Second)
+	log.Print("done")
+
+	// wg := sync.WaitGroup{}
+	// wg.Add(1)
+
+	// go func(ctx context.Context) {
+	// 	for {
+	// 		select {
+	// 		case <-ctx.Done():
+	// 			log.Print("done")
+	// 			wg.Done()
+	// 			return
+	// 		case <-time.After(time.Second):
+	// 			log.Print("tick")
+	// 		}
+	// 	}
+	// }(ctx)
+
+	// wg.Wait()
+	// log.Print("main done")
+
+}
--- pkg/search/search.go
+++ pkg/search/search.go
@@ -1,2 +1,70 @@
 package search
 
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+	"sync"
+)
+
+//Resut - discribe the result of one search
+type Result struct {
+	// phrase were looking for
+	Phrase string
+	//whole line in which the entry was found (without \n or \r\n)
+	Line string
+	//line number(starting from 1) in which the entry was found
+	LineNum int64
+	//position(column) number (starting form 1) in which the entry was found
+	ColNum int64
+}
+
+//All - searches all phrase entries in files (text files).
+func All(ctx context.Context, phrase string, files []string) <-chan []Result {
+
+	ch := make(chan []Result)
+	wg := sync.WaitGroup{}
+	ctx, cancel := context.WithCancel(ctx)
+
+	for i := 0; i < len(files); i++ {
+		wg.Add(1)
+
+		go func(ctx context.Context, path string, ch chan<- []Result) {
+			defer wg.Done()
+
+			results := []Result{}
+			data, err := os.ReadFile(path)
+			if err != nil {
+				log.Print("can not open the file:", err)
+			}
+
+			dataStr := string(data)
+			splitData := strings.Split(dataStr, "\n")
+
+			for index, line := range splitData {
+				if strings.Contains(line, phrase) {
+					result := Result{
+						Phrase:  phrase,
+						Line:    line,
+						LineNum: int64(index + 1),
+						ColNum:  int64(strings.Index(line, phrase) + 1),
+					}
+					results = append(results, result)
+				}
+			}
+
+			if len(results) > 0 {
+				ch <- results
+			}
+		}(ctx, files[i], ch)
+	}
+
+	go func() {
+		defer close(ch)
+		wg.Wait()
+	}()
+
+	cancel()
+	return ch
+}

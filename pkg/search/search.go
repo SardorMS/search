bb3d5e0163af98eaa81e5fc1cb73666998549c11
SardorMS <mshinodawindows@gmail.com>
2021-05-03 17:17:18 +0500

    added Any function and autotest for it

--- pkg/search/search.go
+++ pkg/search/search.go
@@ -68,3 +68,54 @@ func All(ctx context.Context, phrase string, files []string) <-chan []Result {
 	cancel()
 	return ch
 }
+
+func Any(ctx context.Context, phrase string, files []string) <-chan []Result {
+
+	ch := make(chan []Result)
+	wg := sync.WaitGroup{}
+	ctx, cancel := context.WithCancel(ctx)
+
+	for i := 0; i < len(files); i++ {
+		wg.Add(1)
+
+		go func(ctx context.Context, path string, ch chan<- []Result) {
+			defer wg.Done()
+
+			results := []Result{}
+			data, err := os.ReadFile(path)
+			if err != nil {
+				log.Print("can not open the file:", err)
+			}
+
+			dataStr := string(data)
+			splitData := strings.Split(dataStr, "\n")
+
+			for index, line := range splitData {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					if strings.Contains(line, phrase) {
+						result := Result{
+							Phrase:  phrase,
+							Line:    line,
+							LineNum: int64(index + 1),
+							ColNum:  int64(strings.Index(line, phrase) + 1),
+						}
+						results = append(results, result)
+						ch <- results
+						cancel()
+					}
+				}
+			}
+		}(ctx, files[i], ch)
+	}
+
+	go func() {
+		defer close(ch)
+		wg.Wait()
+		cancel()
+	}()
+
+	return ch
+}
